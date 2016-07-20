78533e635931e8956610dea76088c82408e5c2ba
Simon Garnier <garnier@njit.edu>
2016-07-20 15:58:54 +1000

    Should fix missing shared library on Linux.

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(lib, pkg) {
-  if(.Platform$OS.type == "windows"){
+  if (.Platform$OS.type == "windows") {
     pkgPath <- find.package("ROpenCVLite")
     opath <- Sys.getenv("PATH")
     if (.Platform$r_arch == "i386") {
@@ -10,10 +10,16 @@
     binPath <- shortPathName(paste0(pkgPath, binPath))
     Sys.setenv(PATH = paste(binPath, opath, sep=";"))
   }
+
+  if (Sys.info()[1] == "Linux") {
+    pkgPath <- find.package("ROpenCVLite")
+    libPath <- paste0(pkgPath, "/opencv/lib")
+    Sys.setenv(LD_LIBRARY_PATH = paste0(Sys.getenv("LD_LIBRARY_PATH"), ":", libPath))
+  }
 }
 
 .onAttach <- function(lib, pkg) {
-  if(.Platform$OS.type == "windows"){
+  if (.Platform$OS.type == "windows") {
     pkgPath <- find.package("ROpenCVLite")
     opath <- Sys.getenv("PATH")
     if (.Platform$r_arch == "i386") {
@@ -23,5 +29,11 @@
     }
     binPath <- shortPathName(paste0(pkgPath, binPath))
     Sys.setenv(PATH = paste(binPath, opath, sep=";"))
+  }
+
+  if (Sys.info()[1] == "Linux") {
+    pkgPath <- find.package("ROpenCVLite")
+    libPath <- paste0(pkgPath, "/opencv/lib")
+    Sys.setenv(LD_LIBRARY_PATH = paste0(Sys.getenv("LD_LIBRARY_PATH"), ":", libPath))
   }
 }
