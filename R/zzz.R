3157e31c71cab5829a688277f938e1ec44ac40d0
Simon Garnier <garnier@njit.edu>
2019-07-28 14:32:37 +0200

    Fixing errors detected by CRAN

--- R/zzz.R
+++ R/zzz.R
@@ -1,19 +1,21 @@
 .onLoad <- function(lib, pkg) {
   if (.Platform$OS.type == "windows") {
     pkgPath <- find.package("ROpenCVLite")
+    installPath <- gsub("ROpenCVLite", "", pkgPath)
     opath <- Sys.getenv("PATH")
     if (.Platform$r_arch == "i386") {
       binPath <- "/opencv/x86/mingw/bin"
     } else {
       binPath <- "/opencv/x64/mingw/bin"
     }
-    binPath <- utils::shortPathName(paste0(pkgPath, binPath))
+    binPath <- utils::shortPathName(paste0(installPath, binPath))
     Sys.setenv(PATH = paste(binPath, opath, sep = ";"))
   }
 
   if (Sys.info()[["sysname"]] == "Linux") {
     pkgPath <- find.package("ROpenCVLite")
-    libPath <- paste0(pkgPath, "/opencv/lib")
+    installPath <- gsub("ROpenCVLite", "", pkgPath)
+    libPath <- paste0(installPath, "/opencv/lib")
     Sys.setenv(LD_LIBRARY_PATH = paste0(Sys.getenv("LD_LIBRARY_PATH"), ":", libPath))
   }
 }
@@ -21,23 +23,31 @@
 .onAttach <- function(lib, pkg) {
   if (.Platform$OS.type == "windows") {
     pkgPath <- find.package("ROpenCVLite")
+    installPath <- gsub("ROpenCVLite", "", pkgPath)
     opath <- Sys.getenv("PATH")
     if (.Platform$r_arch == "i386") {
       binPath <- "/opencv/x86/mingw/bin"
     } else {
       binPath <- "/opencv/x64/mingw/bin"
     }
-    binPath <- utils::shortPathName(paste0(pkgPath, binPath))
+    binPath <- utils::shortPathName(paste0(installPath, binPath))
     Sys.setenv(PATH = paste(binPath, opath, sep = ";"))
   }
 
   if (Sys.info()[["sysname"]] == "Linux") {
     pkgPath <- find.package("ROpenCVLite")
-    libPath <- paste0(pkgPath, "/opencv/lib")
+    installPath <- gsub("ROpenCVLite", "", pkgPath)
+    libPath <- paste0(installPath, "/opencv/lib")
     Sys.setenv(LD_LIBRARY_PATH = paste0(Sys.getenv("LD_LIBRARY_PATH"), ":", libPath))
   }
 
   if (!ROpenCVLite::isOpenCVInstalled()) {
     installOpenCV()
+  } else {
+    pkgVersion <- unlist(strsplit(as.character(utils::packageVersion("ROpenCVLite")), "\\."))[3]
+    cvVersion <- gsub("\\D+", "", ROpenCVLite::opencvVersion())
+    if (pkgVersion != cvVersion) {
+      installOpenCV()
+    }
   }
 }
