1782ae67eb6d9488f2d7a6d43f4949ac64327813
Simon Garnier <garnier@njit.edu>
2019-12-19 08:58:08 +0100

    Upgrade to OpenCV 4.1.2.

--- R/opencv.R
+++ R/opencv.R
@@ -22,12 +22,12 @@ isOpenCVInstalled <- function() {
 #'
 #' @description This function will attempt to download, compile and install
 #'  OpenCV on the system. This process will take several minutes.
-#' 
-#' @param batch A boolean indicating whether to skip (\code{TRUE}) or not (\code{FALSE}, 
-#'  the default) the interactive installation dialog. This is useful when OpenCV needs  
-#'  to be installed in a non-interactive environment (e.g., during a batch installation 
-#'  on a server). 
-#' 
+#'
+#' @param batch A boolean indicating whether to skip (\code{TRUE}) or not (\code{FALSE},
+#'  the default) the interactive installation dialog. This is useful when OpenCV needs
+#'  to be installed in a non-interactive environment (e.g., during a batch installation
+#'  on a server).
+#'
 #' @return A boolean indicating whether OpenCV was or not installed on the system.
 #'
 #' @author Simon Garnier, \email{garnier@@njit.edu}
@@ -76,17 +76,10 @@ installOpenCV <- function(batch = FALSE) {
       tmpDir <- base::tempdir()
       dir.create(tmpDir, showWarnings = FALSE)
 
-      utils::download.file("https://github.com/opencv/opencv/archive/4.1.0.tar.gz",
-                           paste0(tmpDir, "/opencv-4.1.0.tar.gz"))
-      utils::untar(paste0(tmpDir, "/opencv-4.1.0.tar.gz"),
+      utils::download.file("https://github.com/opencv/opencv/archive/4.1.2.tar.gz",
+                           paste0(tmpDir, "/opencv-4.1.2.tar.gz"))
+      utils::untar(paste0(tmpDir, "/opencv-4.1.2.tar.gz"),
                    exdir = tmpDir)
-
-      file.copy(paste0(pkgPath, "/OpenCVDetectDirectX.4.1.0.cmake"),
-                paste0(tmpDir, "/opencv-4.1.0/cmake/OpenCVDetectDirectX.cmake"),
-                overwrite = TRUE)
-      file.copy(paste0(pkgPath, "/OpenCVDetectOpenCL.4.1.0.cmake"),
-                paste0(tmpDir, "/opencv-4.1.0/cmake/OpenCVDetectOpenCL.cmake"),
-                overwrite = TRUE)
 
       arch <- c("64", "32")
       archAvail <- c(dir.exists(paste0(R.home(), "/bin/x64")),
@@ -98,7 +91,7 @@ installOpenCV <- function(batch = FALSE) {
 
         for (i in 1:2) {
           if (archAvail[i]) {
-            sourceDir <- paste0(tmpDir, "/opencv-4.1.0/")
+            sourceDir <- paste0(tmpDir, "/opencv-4.1.2/")
             buildDir <- paste0(sourceDir, "build", arch[i])
             dir.create(buildDir, showWarnings = FALSE)
             system(paste0('cmake -G "Unix Makefiles" -DCMAKE_C_COMPILER=', rtoolsPath, '/mingw_', arch[i], '/bin/gcc.exe -DCMAKE_CXX_COMPILER=', rtoolsPath, '/mingw_', arch[i], '/bin/g++.exe -DCMAKE_RC_COMPILER=', rtoolsPath, '/mingw_', arch[i], '/bin/windres.exe -DCMAKE_MAKE_PROGRAM=', rtoolsPath, '/mingw_', arch[i], '/bin/mingw32-make.exe -DENABLE_PRECOMPILED_HEADERS=OFF -DENABLE_CXX11=ON -DBUILD_ZLIB=ON -DBUILD_opencv_world=OFF -DBUILD_opencv_contrib_world=OFF -DBUILD_matlab=OFF -DBUILD_opencv_java=OFF -DBUILD_opencv_python2=OFF -DBUILD_opencv_python3=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DWITH_MSMF=OFF -DBUILD_PROTOBUF=OFF -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=', openCVPath, ' -B', buildDir, ' -H', sourceDir))
@@ -114,16 +107,16 @@ installOpenCV <- function(batch = FALSE) {
       tmpDir <- base::tempdir()
       dir.create(tmpDir, showWarnings = FALSE)
 
-      utils::download.file("https://github.com/opencv/opencv/archive/4.1.0.zip",
-                           paste0(tmpDir, "/opencv-4.1.0.zip"))
-      utils::unzip(paste0(tmpDir, "/opencv-4.1.0.zip"),
+      utils::download.file("https://github.com/opencv/opencv/archive/4.1.2.zip",
+                           paste0(tmpDir, "/opencv-4.1.2.zip"))
+      utils::unzip(paste0(tmpDir, "/opencv-4.1.2.zip"),
                    exdir = tmpDir)
 
-      file.copy(paste0(pkgPath, "/OpenCVModule.4.1.0.cmake"),
-                paste0(tmpDir, "/opencv-4.1.0/cmake/OpenCVModule.cmake"),
+      file.copy(paste0(pkgPath, "/OpenCVModule.4.1.2.cmake"),
+                paste0(tmpDir, "/opencv-4.1.2/cmake/OpenCVModule.cmake"),
                 overwrite = TRUE)
 
-      sourceDir <- paste0(tmpDir, "/opencv-4.1.0/")
+      sourceDir <- paste0(tmpDir, "/opencv-4.1.2/")
       buildDir <- paste0(sourceDir, "build")
       dir.create(buildDir, showWarnings = FALSE)
       system(paste0("cmake -DWITH_IPP=ON -DBUILD_opencv_world=OFF -DBUILD_opencv_contrib_world=OFF -DBUILD_opencv_matlab=OFF -DBUILD_opencv_java=OFF -DBUILD_opencv_python2=OFF -DBUILD_opencv_python3=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_TESTS=OFF -DINSTALL_CREATE_DISTRIB=ON -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX=", openCVPath, " -B", buildDir, ' -H', sourceDir))
